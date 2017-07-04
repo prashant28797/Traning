837d8e7d7570561bf1977ea5b565d4d4eed72689
prashant28797 <prashant28797@gmail.com>
2017-07-04 17:21:01 +0530

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,19 @@
+class Third{  
+static int count=1;
+ Third()
+{
+count++;
+System.out.println(count);
+}
+
+ public static void main(String a[]){  
+
+Third s1=new Third();
+
+Third s2=new Third();
+
+Third s3=new Third();
+
+}
+
+} 
