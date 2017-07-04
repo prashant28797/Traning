55c27a90a3924918546d8ea41644c118af3accb5
prashant28797 <prashant28797@gmail.com>
2017-07-04 17:19:20 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,12 @@
+class Odd{  
+    public static void main(String a[]){  
+     int i=0;
+     int num=Integer.parseInt(a[0]);
+     if(i%2!=0)
+{
+   for(i=1;j<=num;i++)
+
+  System.out.println(i);
+}
+    }  
+}  
