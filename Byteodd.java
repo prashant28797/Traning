71d5e3500567621fb6a8cee50de9b5cafc5cf535
prashant28797 <prashant28797@gmail.com>
2017-07-04 17:20:06 +0530

    Add files via upload

---
+++ Byteodd.java
@@ -0,0 +1,12 @@
+class Byteodd{  
+    public static void main(String a[]){  
+     byte i=0;
+     byte num=Byte.parseByte(a[0]);
+     if(i%2!=0)
+{
+   for(i=1;j<=num;i++)
+
+  System.out.println(i);
+}
+    }  
+}  
